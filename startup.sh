3de02da7630d80c8851d72cbde8dd7e00dfb8fa1
Kevin Per <kevin.per@protonmail.com>
2024-04-09 05:07:34 +0000

    Adapt for clang

--- startup.sh
+++ startup.sh
@@ -4,20 +4,18 @@ set -e
 echo "This is the program which will be run..."
 cat /code/main.c
 
-/opt/riscv-cross/bin/riscv32-unknown-linux-gnu-gcc -c -O2 -march=rv32im_zicsr -mabi=ilp32 -o /code/main.o /code/main.c
-/opt/riscv-cross/bin/riscv32-unknown-linux-gnu-gcc -c -O2 -march=rv32im_zicsr -mabi=ilp32 -o /code/init.o /helper/init.s
-/opt/riscv-cross/bin/riscv32-unknown-linux-gnu-gcc -c -O2 -march=rv32im_zicsr -mabi=ilp32 -o /code/trap.o /helper/trap.s
-/opt/riscv-cross/bin/riscv32-unknown-linux-gnu-gcc -c -O2 -march=rv32im_zicsr -mabi=ilp32 -o /code/vars.spike.o /helper/vars.spike.s
-/opt/riscv-cross/bin/riscv32-unknown-linux-gnu-gcc -c -O2 -march=rv32im_zicsr -mabi=ilp32 -o /code/common.o /helper/common.c
-/opt/riscv-cross/bin/riscv32-unknown-linux-gnu-gcc -static -nostartfiles -T/helper/link.ld /code/init.o /code/trap.o /code/vars.spike.o /code/common.o /code/main.o -o /code/main
-
-/opt/riscv-cross/bin/riscv32-unknown-linux-gnu-objdump -d /code/main
+clang-17 -c -O0 -fno-omit-frame-pointer --target=riscv32 -march=rv32im -o /code/main.o /code/main.c
+clang-17 -c -O0 -fno-omit-frame-pointer --target=riscv32 -march=rv32im -o /code/init.o /helper/init.s
+clang-17 -c -O0 -fno-omit-frame-pointer --target=riscv32 -march=rv32im -o /code/trap.o /helper/trap.s
+clang-17 -c -O0 -fno-omit-frame-pointer --target=riscv32 -march=rv32im -o /code/vars.spike.o /helper/vars.spike.s
+clang-17 -c -O0 -fno-omit-frame-pointer --target=riscv32 -march=rv32im -o /code/common.o /helper/common.c
+clang-17 -static --target=riscv32 -nostdlib -T/helper/link.ld /code/init.o /code/trap.o /code/vars.spike.o /code/common.o /code/main.o -o /code/main
 
 file /code/main
-readelf -h /code/main
+/opt/riscv-cross/bin/riscv32-unknown-linux-gnu-objdump -d /code/main
 
 echo "Compilation was ok"
 echo "Running spike..."
 
-/opt/spike/bin/spike --isa=rv32im /code/main
+/opt/spike/bin/spike -d --isa=rv32im /code/main
 echo "Status Code is $?"
